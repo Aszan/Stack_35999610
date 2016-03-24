a3b098c70e53ec3fc732370cb064834439c37907
Aszan <aszan06@gmail.com>
2016-03-25 00:44:00 +0100

    initial commit

---
+++ Stack_35999610/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Stack_35999610
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        #region DATA TRANSFER
+
+        private DataTransferManager _dataTransferManager;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+           base.OnNavigatedTo(e);
+
+            // Register the current page as a share source.
+            _dataTransferManager = DataTransferManager.GetForCurrentView();
+            _dataTransferManager.DataRequested += OnDataRequested;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            // Unregister the current page as a share source.
+            _dataTransferManager.DataRequested -= OnDataRequested;
+
+            base.OnNavigatedFrom(e);
+        }
+
+        private async void OnDataRequested(DataTransferManager sender, DataRequestedEventArgs e)
+        {
+            DataPackage data = e.Request.Data;
+            data.Properties.Title = "App_Name";
+            data.Properties.Description = "App_Description";
+
+            DataRequestDeferral deferral = e.Request.GetDeferral();
+            try
+            {
+                StorageFile thumbnailFile = await Package.Current.InstalledLocation.GetFileAsync("Assets\\StoreLogo.scale-100.png");
+               data.Properties.Thumbnail = RandomAccessStreamReference.CreateFromFile(thumbnailFile);
+
+                StorageFile imageFile = await Package.Current.InstalledLocation.GetFileAsync("Assets\\StoreLogo.png");
+                data.SetBitmap(RandomAccessStreamReference.CreateFromFile(imageFile));
+            }
+            finally
+            {
+                deferral.Complete();
+            }
+        }
+
+        #endregion // DATA TRANSFER
+
+        private void OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
+        }
+    }
+}
